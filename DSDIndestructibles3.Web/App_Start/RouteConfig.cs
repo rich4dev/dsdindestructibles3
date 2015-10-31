fda08c3d3c6177d5bf0836aa9c2f7e4c636bbdbd
rich4dev <ricardosanchezc10@gmail.com>
2015-10-31 07:58:24 -0500

    fsd

--- DSDIndestructibles3.Web/App_Start/RouteConfig.cs
+++ DSDIndestructibles3.Web/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace DSDIndestructibles3.Web
             routes.MapPageRoute("Route1", "RegistrarKitBienvenida", "~/forms/procesos/EntregarKit.aspx");
             routes.MapPageRoute("Route2", "ProgramarInstalacion", "~/forms/procesos/ProgramarInstalacion.aspx");
             routes.MapPageRoute("Route3", "RegistrarInstalacion", "~/forms/procesos/RegistrarInstalacion.aspx");
+            routes.MapPageRoute("Route4", "EvaluarRiesgoFraude", "~/forms/procesos/EvaluarRiesgoFraude.aspx");
         }
     }
 }
---
+++ DSDIndestructibles3.Web/forms/procesos/EvaluarRiesgoFraude.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EvaluarRiesgoFraude.aspx.cs" Inherits="DSDIndestructibles3.Web.forms.procesos.EvaluarRiesgoFraude" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
