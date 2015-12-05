b3ae69bd98a177f631be7be598305a7ff229e18b
dantemejia <dantemejiaalvarado@yahoo.com>
2015-12-04 23:20:37 -0500

    nueva entidad
    se creo dto insp

---
+++ DSDIndestructibles3.Entities/InspeccionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSDIndestructibles3.Entities
+{
+    class InspeccionDTO
+    {
+        public int Id { get; set; }
+        // test github
+    }
+}
